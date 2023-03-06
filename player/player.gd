67037392d2582ed03a0bfb2fbc53494e29b916fa
leosefcik <leosefcik@gmail.com>
2023-03-06 22:42:53 +0100

    Added tilestes and stuf

---
+++ player/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const SPEED := 150.0
+
+func _physics_process(delta):
+	
+	var direction := Input.get_vector("foot_left", "foot_right", "foot_up", "foot_down")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity = Vector2(0,0)
+	
+	look_at(get_global_mouse_position())
+
+	move_and_slide()
