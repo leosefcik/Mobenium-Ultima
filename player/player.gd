4c668d89c4ee1425a35c9e8af71ca2aa1415f848
leosefcik <leosefcik@gmail.com>
2023-03-12 22:07:26 +0100

    +

--- player/player.gd
+++ player/player.gd
@@ -2,7 +2,14 @@ extends CharacterBody2D
 
 const SPEED := 150.0
 
+var testingobject = preload("res://objects/test.tscn")
+
 func _physics_process(delta):
+	if Input.is_action_just_pressed("fire_primary"):
+		var t = testingobject.instantiate()
+		t.rotation = rotation
+		t.position = position
+		get_parent().add_child(t)
 	
 	var direction := Input.get_vector("foot_left", "foot_right", "foot_up", "foot_down")
 	if direction:
